bdfa32a6bd17a78095e70d812d2c70aa27e68e05
kwakJakyeong <dev.ripetangerine@gmail.com>
2026-04-01 12:16:01 +0900

    feat: main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+
+// struct Log{
+//     level: String, // &str
+//     method: String,
+//     path: String
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let log_file_path: &str = "log.txt";
+    let content: String = fs::read_to_string(log_file_path).expect("REASON");
+
+    for line in content.lines(){
+        if line.contains("ERROR"){ println!("{}", line); }
+    }
+
+    println!("--end main--");
 }
